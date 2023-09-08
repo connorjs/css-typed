ea0d654b1824edc6623a6486430bfd3682404532
Connor Sullivan <dev.connorjs@gmail.com>
2023-09-07 20:16:54 -0400

    🎨 Prettier v3
    Upgrades to Prettier v3 and adds `format:fix` run script.

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,3 @@
 module.exports = {
-	trailingComma: `all`, // Default in Prettier 3
 	useTabs: true, // https://alexandersandberg.com/articles/default-to-tabs-instead-of-spaces-for-an-accessible-first-environment/
 };
