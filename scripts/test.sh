ce12d09c1d4c66432a80f869698086edc52cdf50
Connor Sullivan <dev.connorjs@gmail.com>
2024-08-03 17:07:09 -0400

    Fix test case

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,8 +15,9 @@ pushd "$RUNNER_TEMP" > /dev/null || exit
 
 # `./dist/main.js` is executing local `css-typed` as if installed (same as `bin`).
 # But it is `$GITHUB_WORKSPACE/dist/main.js` b/c we `cd $RUNNER_TEMP`.
-echo "css-typed ${2:-} '*.css' ${4:-}"
-eval "$GITHUB_WORKSPACE/dist/main.js ${2:-} '*.css' ${4:-}"
+args="${2:-} '*.css' ${4:-}"
+echo "css-typed $args"
+eval "$GITHUB_WORKSPACE/dist/main.js $args"
 
 # Use `diff` to compare the files.
 # Use `-I '//.*'` to ignore the first line (comment) which has generated path and timestamp.
