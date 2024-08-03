33e1e6e07b866acaf8e5ee47218074e74d437367
Connor Sullivan <dev.connorjs@gmail.com>
2024-08-03 18:08:04 -0400

    add pipeline tests for file config after test refactor

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,14 +3,14 @@
 # $1 is the input name, relative to `fixtures`. Required.
 input=$1
 
-# $2 is the standard (before) options. Defaults to "".
-IFS=" " read -r -a beforeOpts <<< "${2:-}"
+# $2 is the config file name, relative to `fixtures/config`. Defaults to $1.
+config=${2:$1}
 
-# $3 is the output name, relative to `fixtures`. Defaults to $1.
+# $3 is the options. Defaults to "".
+IFS=" " read -r -a options <<< "${2:-}"
+
+# $4 is the output name, relative to `fixtures`. Defaults to $1.
 output=${3:-$1}
-
-# $4 is the after options. Use an array. Defaults to "".
-IFS=" " read -r -a afterOpts <<< "${4:-}"
 
 # $5 is the path prefix for output. Defaults to "".
 prefix=${5:-}
@@ -18,13 +18,14 @@ prefix=${5:-}
 # Run from $RUNNER_TEMP for auto-cleanup.
 cp fixtures/${input}.css $RUNNER_TEMP/test.css
 cp fixtures/${output}.d.css.ts $RUNNER_TEMP/expected.d.css.ts
+cp fixtures/config/${config}.yaml $RUNNER_TEMP/.config/csstypedrc.yaml 2> /dev/null
 pushd $RUNNER_TEMP > /dev/null || exit
 
 # `./dist/main.js` is executing local `css-typed` as if installed (same as `bin`).
 # But it is `$GITHUB_WORKSPACE/dist/main.js` b/c we `cd $RUNNER_TEMP`.
-echo "css-typed ${beforeOpts[*]} \"*.css\" ${afterOpts[*]}"
+echo "css-typed ${options[*]}"
 # shellcheck disable=SC2068
-$GITHUB_WORKSPACE/dist/main.js ${beforeOpts[@]} "*.css" ${afterOpts[@]}
+$GITHUB_WORKSPACE/dist/main.js ${options[@]}
 
 # Use `diff` to compare the files.
 # Use `-I '//.*'` to ignore the first line (comment) which has generated path and timestamp.
