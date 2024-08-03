bc1210a09e2d4c99239bde26e7b4c6d3047663ac
Connor Sullivan <dev.connorjs@gmail.com>
2024-08-03 17:32:53 -0400

    debugging

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
+set -x
 
 # $1 is the input name, relative to `fixtures`. Required.
 input=$1
 
 # $2 is the standard (before) options. Use an array. Defaults to ().
-beforeOpts=${2:-()}
+beforeOpts=${2:-}
 
 # $3 is the output name, relative to `fixtures`. Defaults to $1.
-output=$2
+output=${3:-$1}
 
 # $4 is the after options. Use an array. Defaults to ().
-afterOpts=${4:-()}
+afterOpts=${4:-}
 
 # $5 is the path prefix for output. Defaults to "".
 prefix=${5:-}
@@ -24,7 +25,7 @@ pushd $RUNNER_TEMP > /dev/null || exit
 # But it is `$GITHUB_WORKSPACE/dist/main.js` b/c we `cd $RUNNER_TEMP`.
 echo "css-typed ${beforeOpts} \"*.css\" ${afterOpts}"
 # shellcheck disable=SC2068
-$GITHUB_WORKSPACE/dist/main.js ${beforeOpts[@]} \"*.css\" ${afterOpts[@]}
+$GITHUB_WORKSPACE/dist/main.js ${beforeOpts[@]} "*.css" ${afterOpts[@]}
 
 # Use `diff` to compare the files.
 # Use `-I '//.*'` to ignore the first line (comment) which has generated path and timestamp.
