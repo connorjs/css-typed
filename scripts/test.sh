f87693884d8facc287d935bc9d21d2c6188215e3
Connor Sullivan <dev.connorjs@gmail.com>
2024-08-04 01:06:46 -0400

    ✨ Generate subcommand
    Adds `gen` subcommand (aliased with `generate`). Preserves command-less
    invocation as generate.
    
    Tweaks pipeline tests to allow for new `gen` subcommand.
    
    semver:minor

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,20 +2,23 @@
 set -eo pipefail # Removed `-u` which failed on macos for `options`
 IFS=$' ' # We want space splitting for this script
 
-# $1 is the input name, relative to `fixtures`. Required.
-input=$1
+# $1 is the subcommand to run
+subcommand=$1
 
-# $2 is the config file name, relative to `fixtures/config`. Defaults to $1.yaml.
-config=${2:-$1.yaml}
+# $2 is the input name, relative to `fixtures`. Required.
+input=$2
 
-# $3 is the options. Defaults to "".
-read -r -a options <<< "${3:-}"
+# $3 is the config file name, relative to `fixtures/config`. Defaults to $2.yaml.
+config=${3:-$2.yaml}
 
-# $4 is the output name, relative to `fixtures`. Defaults to $1.
-output=${4:-$1}
+# $4 is the options. Defaults to "".
+read -r -a options <<< "${4:-}"
 
-# $5 is the path prefix for output. Defaults to "".
-prefix=${5:-}
+# $5 is the output name, relative to `fixtures`. Defaults to $1.
+output=${5:-$2}
+
+# $6 is the path prefix for output. Defaults to "".
+prefix=${6:-}
 
 # Run from $RUNNER_TEMP for auto-cleanup.
 cp fixtures/${input}.css $RUNNER_TEMP/test.css
@@ -31,9 +34,9 @@ pushd $RUNNER_TEMP > /dev/null || exit
 
 # `./dist/main.js` is executing local `css-typed` as if installed (same as `bin`).
 # But it is `$GITHUB_WORKSPACE/dist/main.js` b/c we `cd $RUNNER_TEMP`.
-echo "css-typed " "${options[@]}"
+echo "css-typed ${subcommand}" "${options[@]}"
 # shellcheck disable=SC2068
-$GITHUB_WORKSPACE/dist/main.js ${options[@]}
+$GITHUB_WORKSPACE/dist/main.js ${subcommand} ${options[@]}
 
 # Use `diff` to compare the files.
 # Use `-I '//.*'` to ignore the first line (comment) which has generated path and timestamp.
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,16 @@ import { version } from "./version.ts";
 await createCssTypedCommand()
 	.name(`css-typed`)
 	.description(
-		`TypeScript declaration generator for CSS files (and other stylesheets).`,
+		`TypeScript declaration generator for CSS files (and other stylesheets).\n\nRuns \`generate\` when no subcommand given.`,
 	)
 	.version(version)
 	.cssTypedAction(generate)
+	.helpCommand(`help [command]`, `Displays help for the given command.`)
+	.addCommand(
+		createCssTypedCommand()
+			.name(`gen`)
+			.alias(`generate`)
+			.description(`Generates TypeScript declaration files.`)
+			.cssTypedAction(generate),
+	)
 	.parseAsync();
