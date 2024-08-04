e9deffc09c9bf1135ccde79806703b003cda6921
Connor Sullivan <dev.connorjs@gmail.com>
2024-08-04 13:43:21 -0400

    Revert some test changes

--- scripts/test-gen.sh
+++ scripts/test-gen.sh
@@ -2,23 +2,20 @@
 set -eo pipefail # Removed `-u` which failed on macos for `options`
 IFS=$' ' # We want space splitting for this script
 
-# $1 is the subcommand to run
-subcommand=$1
+# $1 is the input name, relative to `fixtures`. Required.
+input=$1
 
-# $2 is the input name, relative to `fixtures`. Required.
-input=$2
+# $2 is the config file name, relative to `fixtures/config`. Defaults to $1.yaml.
+config=${2:-$1.yaml}
 
-# $3 is the config file name, relative to `fixtures/config`. Defaults to $2.yaml.
-config=${3:-$2.yaml}
+# $3 is the options. Defaults to "".
+read -r -a options <<< "${3:-}"
 
-# $4 is the options. Defaults to "".
-read -r -a options <<< "${4:-}"
+# $4 is the output name, relative to `fixtures`. Defaults to $1.
+output=${4:-$1}
 
-# $5 is the output name, relative to `fixtures`. Defaults to $1.
-output=${5:-$2}
-
-# $6 is the path prefix for output. Defaults to "".
-prefix=${6:-}
+# $5 is the path prefix for output. Defaults to "".
+prefix=${5:-}
 
 # Run from $RUNNER_TEMP for auto-cleanup.
 cp fixtures/${input}.css $RUNNER_TEMP/test.css
@@ -34,9 +31,9 @@ pushd $RUNNER_TEMP > /dev/null || exit
 
 # `./dist/main.js` is executing local `css-typed` as if installed (same as `bin`).
 # But it is `$GITHUB_WORKSPACE/dist/main.js` b/c we `cd $RUNNER_TEMP`.
-echo "css-typed ${subcommand}" "${options[@]}"
+echo "css-typed gen" "${options[@]}"
 # shellcheck disable=SC2068
-$GITHUB_WORKSPACE/dist/main.js ${subcommand} ${options[@]}
+$GITHUB_WORKSPACE/dist/main.js gen ${options[@]}
 
 # Use `diff` to compare the files.
 # Use `-I '//.*'` to ignore the first line (comment) which has generated path and timestamp.
